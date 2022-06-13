69e5448e714f5c1c7f1f12988ba5465fd41c3b28
amietti <52357776+ammipw@users.noreply.github.com>
2022-06-13 23:30:05 +0100

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bf221406ae437498"; }
+/******/ 		__webpack_require__.h = function() { return "90ae1960328138ab"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- templates/notflix/index.js
+++ templates/notflix/index.js
@@ -1,14 +1,22 @@
 import Head from 'next/head'
-import styles from './notflix.module.css'
+import { useState } from 'react'
+import styles from './notflix.module.scss'
 
 export default function Notflix(){
+    const [active, setActive] = useState(null)
+
+    function handleHover(e){
+        e.target.className += ' ' + styles.active
+    }
+
     return (
         <div className={styles.document}>
             <Head>
                 <title>Notflix - Gif library</title>
             </Head>
             <div className={styles.body}>
-
+                <div className={styles.box} style={{background: 'lightgreen'}} onMouseOver={handleHover}></div>
+                <div className={styles.box} style={{background: 'lightblue'}} onMouseOver={handleHover}></div>
             </div>
         </div>
     )
